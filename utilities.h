5aead517f044ef9c4d1e4cf7f58f69e495377ec5
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2022-06-16 19:21:19 -0300

    create: Criado header contendo declarações de funções e TADS

--- utilities.h
+++ utilities.h
@@ -1,11 +1,19 @@
-#include <stdio.h>
-#include <errno.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-
+#include <errno.h>
+#include <stdio.h>
+#include <time.h>
 
 typedef struct Pagina {
 	char address[9];
 	struct Pagina *next;	
-}Pagina;
+} Pagina;
+
+void Add_page(char value[9]);
+void LRU(char value[9]);
+void NRU(char value[9]);
+void SecondChance(char value[9]);
+bool Find(char value[9]);
+void Tipo_alg(char value[9]);
+void WriteAddress(char value[9]);
