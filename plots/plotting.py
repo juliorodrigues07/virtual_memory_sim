d80b2ca411590e3f54035a35e675359eab87b242
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2022-06-21 01:35:31 -0300

    create: Adicionado script exclusivo para import de métodos para realizar plots de gráficos

---
+++ plots/plotting.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+
+
+def plot_graph(x, page_fault, write_backs):
+
+    plt.grid()
+    plt.xlabel('Tamanho da memória principal')
+    plt.ylabel('Quantidade de páginas')
+    plt.plot(x, page_fault, color='b', label='Falta de Páginas')
+    plt.plot(x, write_backs, color='g', label='Páginas "Sujas"')
+    plt.legend(loc='best')
+    plt.show()
+
+
+def plot_page_faults(mem, x, y, z):
+
+    plt.grid()
+    plt.xlabel('Tamanho da memória principal')
+    plt.ylabel('Quantidade de páginas')
+    plt.plot(mem, x, color='b', label='LRU')
+    plt.plot(mem, y, color='g', label='Segunda Chance')
+    plt.plot(mem, z, color='r', label='NRU')
+    plt.legend(loc='best')
+    plt.show()
+
+
+def plot_writebacks(mem, a, b, c):
+
+    plt.grid()
+    plt.xlabel('Tamanho da memória principal')
+    plt.ylabel('Quantidade de páginas')
+    plt.plot(mem, a, color='b', label='LRU')
+    plt.plot(mem, b, color='g', label='Segunda Chance')
+    plt.plot(mem, c, color='r', label='NRU')
+    plt.legend(loc='best')
+    plt.show()
